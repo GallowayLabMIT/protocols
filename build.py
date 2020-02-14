508601cc063e92bb2c887964bc1655c488d190c1
Christopher Johnstone <meson800@gmail.com>
2020-02-14 16:38:43 -0500

    Updated build script to rewrite ToC

--- build.py
+++ build.py
@@ -1,6 +1,57 @@
 import shutil
 import os
 import subprocess
+from bs4 import BeautifulSoup
+
+def update_toc(directory):
+    """
+    Given a directory that contains HTML files,
+    attempts to open and rewrite their ToC's to
+    include links to the other documents.
+    """
+    for root, dirs, files in os.walk(directory):
+        # Ignore subdirs
+        dirs = []
+        # Extract HTML soup of each HTML file
+        soups = {}
+        for filename in [f for f in files if
+                os.path.splitext(f)[1] == '.html']:
+            with open(os.path.join(directory, filename)) as raw_html:
+                soups[filename] = BeautifulSoup(
+                        raw_html,
+                        features='html.parser')
+        
+        
+        # Generate new a-tags for the other files in the same
+        # directory
+        extra_links = []
+        for filename, soup in soups.items():
+            a_tag = soup.find('div', id='sidebar').ul.li.a
+            a_tag['href'] = filename
+            extra_links.append((filename, a_tag))
+
+        # Update the HTML in each entry, using the extra links
+        # After updating the sidebar, write out the HTML
+        for filename, soup in soups.items():
+            sidebar_ul = soup.find('div', id='sidebar').ul
+            before_link = True
+            for insert_filename, link in extra_links:
+                if insert_filename == filename:
+                    # Skip!
+                    before_link = False
+                    continue
+
+                new_tag = soup.new_tag("li");
+                new_tag.append(link)
+
+                if before_link:
+                    sidebar_ul.insert(0, new_tag)
+                else:
+                    sidebar_ul.append(new_tag)
+            with open(os.path.join(directory, filename), 'wb') as f:
+                f.write(soup.prettify('utf-8'))
+        
+
 
 if __name__ == '__main__':
     # Remove the output folder if it exists and recreate it
@@ -36,10 +87,13 @@ if __name__ == '__main__':
         # Convert each md file
         for file in files:
             (filename, extension) = os.path.splitext(file)
+            output_filename = os.path.join(output_path, filename + '.html')
             if extension == '.md':
                 subprocess.run(['pandoc',
                     os.path.join(root, file),
                     '--template=pandoc-toc-sidebar/toc-sidebarL.html',
                     '--self-contained', '--resource-path=pandoc-toc-sidebar',
                     '-B', 'docs/nav', '--toc', '-o',
-                    os.path.join(output_path, filename + '.html')])
+                    output_filename])
+        # Fixup the ToCs
+        update_toc(output_path)
