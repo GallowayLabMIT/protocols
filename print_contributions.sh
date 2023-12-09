5aa99f8c72bbbf3eaa3f3ed29cbee8519ee9dc71
Christopher Johnstone <meson800@gmail.com>
2023-12-08 21:12:55 -0500

    Added additional contributor info scripts

---
+++ print_contributions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+#
+find_contributions() {
+    echo "================================================== $1 ======================================================="
+    git log --reverse --shortstat --pretty=format:"%an (%s)" -- "$1"
+}
+
+export -f find_contributions
+
+git ls-files -z "*.rst" | xargs -0 -I {} bash -c 'find_contributions "{}"' _ {}
